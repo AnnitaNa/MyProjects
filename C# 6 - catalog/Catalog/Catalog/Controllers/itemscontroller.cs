8959d6ec5cfe45eaf9c97bb29aed69a6c1aed4b6
Tatiane A. Nicchetti <tanicchetti@gmail.com>
2022-07-17 14:31:19 -0300

    created repository

---
+++ C# 6 - catalog/Catalog/Catalog/Controllers/itemscontroller.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers;
+
+
+[ApiController]
+[Route("items")]
+public class itemscontroller : ControllerBase
+{
+    private readonly inMemItemsRepository repository;
+
+    public itemscontroller()
+    {
+        repository = new inMemItemsRepository();
+    }
+    
+    //GET /items
+    [HttpGet]
+    public IEnumerable<Item> GetItems()
+    {
+        var item = repository.GetItems();
+        return item;
+    }
+    
+    // NOT WORKING!
+    
+    //GET items/id
+    //[HttpGet("{id}")]
+    //public Item GetItem(Guid id)
+    //{
+      //  var item = repository.GetItem(id);
+        //return item;
+    //}
+
+}
---
+++ C# 6 - catalog/Catalog/Catalog/Repositories/inMemItemsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories;
+
+public class inMemItemsRepository
+{
+    private readonly List<Item> items = new()
+    {
+        new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+        new Item { Id = Guid.NewGuid(), Name = "Sword", Price = 100, CreatedDate = DateTimeOffset.UtcNow },
+        new Item { Id = Guid.NewGuid(), Name = "shield", Price = 50, CreatedDate = DateTimeOffset.UtcNow },
+    };
+
+    public IEnumerable<Item> GetItems()
+    {
+        return items;
+    }
+
+    public Item GetItem(Guid id)
+    {
+        return items.Where(item => item.Id == id).SingleOrDefault();
+    }
+}
